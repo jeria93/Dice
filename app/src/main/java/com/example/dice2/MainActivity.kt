1e0045ffd2313cad3293d9f1af9af072ae05701e
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-11-05 21:08:37 +0100

    Some functionality added - mini game, if user hits 2 (6) in a row you get 1 point.

--- app/src/main/java/com/example/dice2/MainActivity.kt
+++ app/src/main/java/com/example/dice2/MainActivity.kt
@@ -15,6 +15,8 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
 //    lateinit var textView:TextView
+    var score = 0
+    var currentRoll = -1 //No rolls has been made
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +28,14 @@ class MainActivity : AppCompatActivity() {
         val myTextView: TextView
         val myButton: Button
         val imageView: ImageView
+        val scoreTextView: TextView
 
 //        Set them
         myTextView = findViewById(R.id.textView)
         myButton = findViewById(R.id.button)
         imageView = findViewById(R.id.imageView)
+        scoreTextView = findViewById(R.id.textView2)
+
 
 //        Logic
         val imageList = listOf(
@@ -51,13 +56,21 @@ class MainActivity : AppCompatActivity() {
             myTextView.text = "Your random dice number is ${randomNumber + 1}"
             imageView.setImageResource(imageList[randomNumber])
 
+            if (randomNumber == 5) {
+                if (currentRoll == 5) {
+                    score++
+                    Log.d("Score", "score is $score")
+                    scoreTextView.text = "Current score is: $score"
+                }
+            }
+            currentRoll = randomNumber
         }
 
 
 
 
 
-
+//Everytime you hit one 6 after another you get points.
 
 
     }
